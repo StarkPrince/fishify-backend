67ffcaa2c584a7eaa1ec2b80f3003202ef2881f5
StarkPrince <games.princeraj@gmail.com>
2021-11-14 16:54:25 +0530

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ac1b658777025de83323258e0aa732ba'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b29f3138-e2c1-4f07-a2a2-fc5278bb4e59'
+};
